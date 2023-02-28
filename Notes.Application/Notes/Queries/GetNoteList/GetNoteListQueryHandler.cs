b129b890ea06abb26672694b8e72ada9acd30931
Zuguki <guskov.zahar20@gmail.com>
2023-02-28 08:07:10 +0300

    GetNoteList done

---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+
+namespace Notes.Application.Notes.Queries.GetNoteList;
+
+public class GetNoteListQueryHandler : IRequestHandler<GetNoteListQuery, NoteListVm>
+{
+    private INotesDbContext _dbContext;
+    private IMapper _mapper;
+
+    public GetNoteListQueryHandler(INotesDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+    {
+        var notesQuery = await _dbContext.Notes
+            .Where(note => note.UserId == request.UserId)
+            .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new NoteListVm {Notes = notesQuery};
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/NoteListVm.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace Notes.Application.Notes.Queries.GetNoteList;
+
+public class NoteListVm
+{
+    public IList<NoteLookupDto> Notes { get; set; }
+}
